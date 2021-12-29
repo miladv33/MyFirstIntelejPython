28f39ec4719eb34ec8f7bbbc1ea8145b4d60b446
Milad Mosavi <milad.mosavi.developer@gmail.com>
2021-12-29 17:54:52 +0330

    Initial commit

---
+++ first/__init__.py
@@ -0,0 +1,17 @@
+s = 'my name is milad'
+
+print(s.capitalize())
+
+print(s.replace('a', 's'))
+
+print(s.rfind('a'))
+
+numbers = [1, 2, 3]
+numbers2 = list((1, 2, 3))
+print(numbers)
+print(numbers2)
+
+numbers.append(22)
+numbers.remove(22)
+numbers.insert(10, 222)
+print(numbers)
